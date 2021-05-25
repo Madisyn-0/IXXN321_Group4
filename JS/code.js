83912f073d943825814db0b29b082cad99ca3f67
Madisyn-0 <madimuggeridge@gmail.com>
2021-05-25 18:18:02 +1200

    Tried to fix input being empty

--- JS/code.js
+++ JS/code.js
@@ -4,14 +4,21 @@ const inputEmail = document.getElementById('email-input');
 const newsletterDiv = document.getElementById('newsletter-content');
 const label = document.getElementById('email');
 const text =document.getElementById('newsletter-text');
+const input = document.getElementById('email-input');
 
 
  subscribe.addEventListener('click', () => {
-    newsletterHeading.textContent = 'Thank you for subscribing!';
-    email.style.display = 'none';
-    newsletterHeading.style.color = '#1F524F';
-    // email.style.fontSize = '30px';
-    subscribe.style.display = 'none';
-    inputEmail.style.display = 'none';
-    text.textContent = 'We will send you a confirmaiton email';
+   if (input == '') {
+      prompt('Please enter your email address');
+   }else {
+      newsletterHeading.textContent = 'Thank you for subscribing!';
+      email.style.display = 'none';
+      newsletterHeading.style.color = '#1F524F';
+      // email.style.fontSize = '30px';
+      subscribe.style.display = 'none';
+      inputEmail.style.display = 'none';
+      text.textContent = 'We will send you a confirmaiton email';
+   }
+
  });
+
