d888cfb620754367a915941c5d429dfd0fe81054
Madisyn-0 <madimuggeridge@gmail.com>
2021-05-26 16:35:34 +1200

    Fixed javaScript input and organised content
    -  Made the subscribe button only work if the user has entered something
    
    - Cleaned up code and matched it to Figma

--- JS/code.js
+++ JS/code.js
@@ -1,24 +1,20 @@
 const subscribe = document.getElementById('email-button');
 const newsletterHeading = document.getElementById('newsletter');
 const inputEmail = document.getElementById('email-input');
-const newsletterDiv = document.getElementById('newsletter-content');
 const label = document.getElementById('email');
-const text =document.getElementById('newsletter-text');
-const input = document.getElementById('email-input');
-
+const text = document.getElementById('newsletter-text');
 
  subscribe.addEventListener('click', () => {
-   if (input == '') {
-      prompt('Please enter your email address');
+   if (inputEmail.value.length == 0) {
+      label.textContent = 'Please enter your email address';
+      label.style.fontWeight = 700;
    }else {
       newsletterHeading.textContent = 'Thank you for subscribing!';
-      email.style.display = 'none';
+      label.style.display = 'none';
       newsletterHeading.style.color = '#1F524F';
-      // email.style.fontSize = '30px';
       subscribe.style.display = 'none';
       inputEmail.style.display = 'none';
-      text.textContent = 'We will send you a confirmaiton email';
+      text.textContent = `We will send you a confirmaiton email at ${inputEmail.value}`;
    }
 
  });
-
